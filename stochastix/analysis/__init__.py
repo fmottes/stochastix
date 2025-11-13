8425d8efae2700642073a2ee41d84abdb998b862
fmottes <49645353+fmottes@users.noreply.github.com>
2025-11-13 02:36:09 -0500

    Add `kde_triangular_2d` to exports in analysis module's `__init__.py`

--- stochastix/analysis/__init__.py
+++ stochastix/analysis/__init__.py
@@ -3,6 +3,7 @@
 from .corr import autocorrelation, cross_correlation
 from .hist import state_kde
 from .kde_1d import kde_exponential, kde_gaussian, kde_triangular
+from .kde_2d import kde_triangular_2d
 from .mi import mutual_information, state_mutual_info
 
 __all__ = [
@@ -11,6 +12,7 @@ __all__ = [
     'kde_exponential',
     'kde_gaussian',
     'kde_triangular',
+    'kde_triangular_2d',
     'mutual_information',
     'state_kde',
     'state_mutual_info',
