7b819c21cf49a7a178046ce085083349f8756ee4
fmottes <49645353+fmottes@users.noreply.github.com>
2026-02-21 09:27:03 -0500

    update init exports for kde

--- stochastix/analysis/__init__.py
+++ stochastix/analysis/__init__.py
@@ -2,8 +2,13 @@
 
 from .corr import autocorrelation, cross_correlation
 from .hist import state_kde
-from .kde_1d import kde_exponential, kde_gaussian, kde_triangular
-from .kde_2d import kde_triangular_2d
+from .kde_1d import kde_exponential, kde_gaussian, kde_triangular, kde_wendland_c2
+from .kde_2d import (
+    kde_exponential_2d,
+    kde_gaussian_2d,
+    kde_triangular_2d,
+    kde_wendland_c2_2d,
+)
 from .mi import mutual_information, state_mutual_info
 
 __all__ = [
@@ -13,6 +18,10 @@ __all__ = [
     'kde_gaussian',
     'kde_triangular',
     'kde_triangular_2d',
+    'kde_wendland_c2',
+    'kde_wendland_c2_2d',
+    'kde_exponential_2d',
+    'kde_gaussian_2d',
     'mutual_information',
     'state_kde',
     'state_mutual_info',
